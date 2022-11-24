612070c73290341c83fbc1e0bccfd3d90352f893
nontoa <nicolasnc010@gmail.com>
2022-11-23 23:07:06 -0500

    Adding instance commands

--- cdk/src/main/java/com/myorg/CdkStack.java
+++ cdk/src/main/java/com/myorg/CdkStack.java
@@ -24,6 +24,15 @@ public class CdkStack extends Stack {
             .displayName("My First Topic Yeah")
             .build();
 
+
+        //TODO: It's missing to download de docker-compose.yml
+        UserData userDataSCript = UserData.forLinux();
+        userDataSCript.addCommands("sudo yum install docker -y");
+        userDataSCript.addCommands("sudo service docker start");
+        userDataSCript.addCommands("sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose");
+        userDataSCript.addCommands("sudo chmod +x /usr/bin/docker-compose");
+        userDataSCript.addCommands("sudo docker-compose up -d");
+
         topic.addSubscription(new SqsSubscription(queue));
     }
 }
