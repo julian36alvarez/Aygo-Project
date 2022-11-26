16ae00a84e6acd56f9dc8e11bd89025222bc0a82
Julian Benitez Gutierrez <julian.benitezg99@gmail.com>
2022-11-26 09:31:25 -0500

    Fix dir for cdk

--- cdk/src/main/java/com/myorg/CdkStack.java
+++ cdk/src/main/java/com/myorg/CdkStack.java
@@ -88,7 +88,7 @@ public class CdkStack extends Stack {
         userDataScript.addCommands("mvn clean install");
         userDataScript.addCommands("export AWS_ACCESS_KEY_ID=xx");
         userDataScript.addCommands("export AWS_SECRET_ACCESS_KEY=xx");
-        userDataScript.addCommands("java -jar backend/target/SpringVideoApp-1.0-SNAPSHOT.jar --server.port=8080");
+        userDataScript.addCommands("java -jar target/SpringVideoApp-1.0-SNAPSHOT.jar --server.port=8080");
 
 
         Bucket.Builder.create(this, "aygo-bucket-project-test")
