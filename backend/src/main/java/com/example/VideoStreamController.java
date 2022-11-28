edce0a4e9770a84175cb5a54a8a71ae3351859a8
JulianBenitez <julian.benitezg99@gmail.com>
2022-11-28 15:25:45 -0500

    Update backend items

--- backend/src/main/java/com/example/VideoStreamController.java
+++ backend/src/main/java/com/example/VideoStreamController.java
@@ -1,26 +1,23 @@
-/*
-   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   SPDX-License-Identifier: Apache-2.0
-*/
-
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import reactor.core.publisher.Mono;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class VideoStreamController {
@@ -30,9 +27,9 @@ public class VideoStreamController {
 
     private String bucket = "aygo-bucket-project-test";
 
-    @RequestMapping(value = "/")
-    public String root() {
-        return "index";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("Welcome to the Video Streamer!");
     }
 
     @GetMapping("/watch")
@@ -52,8 +49,8 @@ public class VideoStreamController {
 
         try {
             byte[] bytes = file.getBytes();
-            String name = file.getOriginalFilename() ;
-            String desc2 = description ;
+            String name = file.getOriginalFilename();
+            String desc2 = description;
 
             // Put the MP4 file into an Amazon S3 bucket.
             vid.putVideo(bytes, bucket, name, desc2);
@@ -67,10 +64,8 @@ public class VideoStreamController {
     // Returns items to populate the Video menu.
     @RequestMapping(value = "/items", method = RequestMethod.GET)
     @ResponseBody
-    public String getItems(HttpServletRequest request, HttpServletResponse response) {
-
-        String xml = vid.getTags(bucket);
-        return xml;
+    public List<Tag> getItems(HttpServletRequest request, HttpServletResponse response) {
+        return vid.getTags(bucket);
     }
 
     // Returns the video in the bucket specified by the ID value.
--- backend/src/main/java/com/example/VideoStreamService.java
+++ backend/src/main/java/com/example/VideoStreamService.java
@@ -1,8 +1,3 @@
-/*
-   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   SPDX-License-Identifier: Apache-2.0
-*/
-
 package com.example;
 
 import org.springframework.http.HttpStatus;
@@ -13,29 +8,18 @@ import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
-import software.amazon.awssdk.services.s3.model.S3Object;
-import software.amazon.awssdk.services.s3.model.GetObjectTaggingRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectTaggingResponse;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-import software.amazon.awssdk.services.s3.model.Tag;
-import org.w3c.dom.Document;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Element;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.StringWriter;
-import java.util.List;
+import software.amazon.awssdk.services.s3.model.GetObjectTaggingRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectTaggingResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VideoStreamService {
@@ -43,7 +27,7 @@ public class VideoStreamService {
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String CONTENT_LENGTH = "Content-Length";
     public static final String VIDEO_CONTENT = "video/";
-    private S3Client s3 = null ;
+    private S3Client s3 = null;
 
     private S3Client getClient() {
 
@@ -63,7 +47,7 @@ public class VideoStreamService {
 
         try {
             // Set the tags to apply to the object.
-            String theTags = "name="+fileName+"&description="+description;
+            String theTags = "name=" + fileName + "&description=" + description;
 
             PutObjectRequest putOb = PutObjectRequest.builder()
                     .bucket(bucketName)
@@ -80,80 +64,80 @@ public class VideoStreamService {
     }
 
     // Returns a schema that describes all tags for all videos in the given bucket.
-    public String getTags(String bucketName){
-      s3 = getClient();
+    public List<Tag> getTags(String bucketName) {
+        s3 = getClient();
 
-      try {
+        try {
 
-          ListObjectsRequest listObjects = ListObjectsRequest
-                  .builder()
-                  .bucket(bucketName)
-                  .build();
+            ListObjectsRequest listObjects = ListObjectsRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .build();
 
-          ListObjectsResponse res = s3.listObjects(listObjects);
-          List<S3Object> objects = res.contents();
-          List<String> keys = new ArrayList<>();
+            ListObjectsResponse res = s3.listObjects(listObjects);
+            List<S3Object> objects = res.contents();
+            List<String> keys = new ArrayList<>();
 
-         for (S3Object myValue: objects) {
+            for (S3Object myValue : objects) {
 
-              String key = myValue.key(); // We need the key to get the tags.
+                String key = myValue.key(); // We need the key to get the tags.
 
-              //Get the tags.
-              GetObjectTaggingRequest getTaggingRequest = GetObjectTaggingRequest
-                      .builder()
-                      .key(key)
-                      .bucket(bucketName)
-                      .build();
+                //Get the tags.
+                GetObjectTaggingRequest getTaggingRequest = GetObjectTaggingRequest
+                        .builder()
+                        .key(key)
+                        .bucket(bucketName)
+                        .build();
 
-              GetObjectTaggingResponse tags = s3.getObjectTagging(getTaggingRequest);
-              List<Tag> tagSet= tags.tagSet();
-              for (Tag tag : tagSet) {
-                 keys.add(tag.value());
-              }
-          }
+                GetObjectTaggingResponse tags = s3.getObjectTagging(getTaggingRequest);
+                List<software.amazon.awssdk.services.s3.model.Tag> tagSet = tags.tagSet();
+                for (software.amazon.awssdk.services.s3.model.Tag tag : tagSet) {
+                    keys.add(tag.value());
+                }
+            }
 
-          List<Tags> tagList = modList(keys);
-          return convertToString(toXml(tagList));
+            List<Tag> tagList = modList(keys);
+            return tagList;
 
-    } catch (S3Exception e) {
-        System.err.println(e.awsErrorDetails().errorMessage());
-        System.exit(1);
-    }
-        return "";
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        return new ArrayList<>();
     }
 
     // Return a List where each element is a Tags object.
-    private List<Tags> modList(List<String> myList){
+    private List<Tag> modList(List<String> myList) {
 
         // Get the elements from the collection.
         int count = myList.size();
-        List<Tags> allTags = new ArrayList<>();
-        Tags myTag ;
+        List<Tag> allTags = new ArrayList<>();
+        Tag myTag;
         ArrayList<String> keys = new ArrayList<>();
         ArrayList<String> values = new ArrayList<>();
 
-        for ( int index=0; index < count; index++) {
+        for (int index = 0; index < count; index++) {
 
             if (index % 2 == 0)
                 keys.add(myList.get(index));
             else
                 values.add(myList.get(index));
-           }
+        }
 
-           // Create a list where each element is a Tags object.
-           for (int r=0; r<keys.size(); r++){
+        // Create a list where each element is a Tags object.
+        for (int r = 0; r < keys.size(); r++) {
 
-               myTag = new Tags();
-               myTag.setName(keys.get(r));
-               myTag.setDesc(values.get(r));
-               allTags.add(myTag);
-           }
+            myTag = new Tag();
+            myTag.setName(keys.get(r));
+            myTag.setDesc(values.get(r));
+            allTags.add(myTag);
+        }
         return allTags;
     }
 
 
     // Reads a video from a bucket and returns a ResponseEntity.
-    public ResponseEntity<byte[]> getObjectBytes (String bucketName, String keyName) {
+    public ResponseEntity<byte[]> getObjectBytes(String bucketName, String keyName) {
 
         s3 = getClient();
 
@@ -179,54 +163,4 @@ public class VideoStreamService {
         return null;
     }
 
-
-    // Converts a list to XML data.
-     private Document toXml(List<Tags> itemList) {
-
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.newDocument();
-
-            // Start building the XML.
-            Element root = doc.createElement( "Tags" );
-            doc.appendChild( root );
-
-            // Iterate through the list.
-            for (Tags myItem: itemList) {
-
-                Element item = doc.createElement( "Tag" );
-                root.appendChild( item );
-
-                // Set Name.
-                Element id = doc.createElement( "Name" );
-                id.appendChild( doc.createTextNode(myItem.getName() ) );
-                item.appendChild( id );
-
-                // Set Description.
-                Element name = doc.createElement( "Description" );
-                name.appendChild( doc.createTextNode(myItem.getDesc() ) );
-                item.appendChild( name );
-            }
-
-            return doc;
-        } catch(ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private String convertToString(Document xml) {
-        try {
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            StreamResult result = new StreamResult(new StringWriter());
-            DOMSource source = new DOMSource(xml);
-            transformer.transform(source, result);
-            return result.getWriter().toString();
-
-        } catch(TransformerException ex) {
-            ex.printStackTrace();
-        }
-        return null;
-    }
 }
