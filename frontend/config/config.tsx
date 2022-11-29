d54787ee31804cbf961e59fd7863e33dbc6c2977
JulianBenitez <julian.benitezg99@gmail.com>
2022-11-28 22:02:01 -0500

    Update url

--- frontend/config/config.tsx
+++ frontend/config/config.tsx
@@ -1,5 +1,5 @@
 const config = {
-  url: 'https://4138rxo925.execute-api.us-east-1.amazonaws.com/aygo-project-deployment'
+  url: 'https://37egbyh6ij.execute-api.us-east-1.amazonaws.com'
 }
 
 export default config
