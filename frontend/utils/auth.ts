6b5ed36c940f7d7fc7b73a1f74177d4488040694
JulianBenitez <julian.benitezg99@gmail.com>
2022-11-29 17:16:28 -0500

    Cognito implemented

---
+++ frontend/utils/auth.ts
@@ -0,0 +1,35 @@
+import config from "../config/config"
+import { CognitoJwtVerifier } from 'aws-jwt-verify'
+
+const getToken = () => {
+  const hash = window.location.hash
+  if (hash) {
+    const token = hash.split('&')[0].split('=')[1]
+    if (token) {
+      localStorage.setItem('aygo-token-aws', token)
+      return token
+    } else {
+      return localStorage.getItem('aygo-token-aws')
+    }
+  }
+}
+
+const validateToken = async (token) => {
+  const verifier = CognitoJwtVerifier.create({
+    userPoolId: `${config.user_pool_id}`,
+    tokenUse: 'id',
+    clientId: `${config.cognito_client_id}`
+  })
+
+  try {
+    console.log('Token', token)
+    const payload = await verifier.verify(token)
+    console.log('Token is valid. Payload', payload)
+    return true
+  } catch (error) {
+    console.log('Token is invalid', error)
+    return false
+  }
+}
+
+export default {getToken, validateToken}
