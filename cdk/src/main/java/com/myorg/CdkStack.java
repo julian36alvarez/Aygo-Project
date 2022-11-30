f2c0ef5d8f4e42c7a4559fad88e99a8147d06103
julian36alvarez <julian36alvarez@gmail.com>
2022-11-29 20:11:07 -0500

    Load Balancer

--- cdk/src/main/java/com/myorg/CdkStack.java
+++ cdk/src/main/java/com/myorg/CdkStack.java
@@ -1,35 +1,23 @@
 package com.myorg;
 
 import software.amazon.awscdk.RemovalPolicy;
-import software.amazon.awscdk.services.apigateway.IntegrationType;
-import software.amazon.awscdk.services.apigatewayv2.alpha.CorsPreflightOptions;
-import software.amazon.awscdk.services.apigatewayv2.alpha.HttpApi;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigatewayv2.alpha.*;
+import software.amazon.awscdk.services.apigatewayv2.integrations.alpha.HttpUrlIntegration;
 import software.amazon.awscdk.services.autoscaling.AutoScalingGroup;
 import software.amazon.awscdk.services.ec2.*;
-import software.amazon.awscdk.services.elasticloadbalancingv2.*;
 import software.amazon.awscdk.services.elasticloadbalancingv2.Protocol;
-import software.amazon.awscdk.services.elasticloadbalancingv2.targets.InstanceTarget;
+import software.amazon.awscdk.services.elasticloadbalancingv2.*;
 import software.amazon.awscdk.services.s3.BlockPublicAccess;
 import software.amazon.awscdk.services.s3.Bucket;
 import software.amazon.awscdk.services.s3.BucketEncryption;
 import software.amazon.awscdk.services.s3.ObjectOwnership;
 import software.constructs.Construct;
-import software.amazon.awscdk.Duration;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
 
-import software.amazon.awscdk.services.stepfunctions.Pass;
-import software.amazon.awscdk.services.stepfunctions.StateMachine;
-import software.amazon.awscdk.services.stepfunctions.StateMachineType;
-import software.amazon.awscdk.services.apigateway.StepFunctionsRestApi;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+
 
 public class CdkStack extends Stack {
     public CdkStack(final Construct parent, final String id) {
@@ -39,32 +27,16 @@ public class CdkStack extends Stack {
     public CdkStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        final Queue queue = Queue.Builder.create(this, "CdkQueue")
-                .visibilityTimeout(Duration.seconds(300))
-                .build();
-
-        final Topic topic = Topic.Builder.create(this, "CdkTopic")
-            .displayName("My First Topic Yeah")
-            .build();
 
         InstanceType instanceType = InstanceType.of(InstanceClass.T2, InstanceSize.MICRO);
 
         IMachineImage machineImage = MachineImage.latestAmazonLinux();
 
-        Vpc vpc = Vpc.Builder.create(this, "VPC")
-                .maxAzs(2)
-                .enableDnsHostnames(true)
-                .enableDnsSupport(true)
-                .build();
+        Vpc vpc = Vpc.Builder.create(this, "VPC").maxAzs(2).enableDnsHostnames(true).enableDnsSupport(true).build();
 
-        SecurityGroup securityGroup = SecurityGroup.Builder.create(this, "SecurityGroup")
-                .vpc(vpc)
-                .allowAllOutbound(true)
-                .build();
+        SecurityGroup securityGroup = SecurityGroup.Builder.create(this, "SecurityGroup").vpc(vpc).allowAllOutbound(true).build();
 
-        SubnetSelection subnetSelection = SubnetSelection.builder()
-                .subnetType(SubnetType.PUBLIC)
-                .build();
+        SubnetSelection subnetSelection = SubnetSelection.builder().subnetType(SubnetType.PUBLIC).build();
 
         securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), "allow SSH access");
         securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(8080), "allow HTTP access");
@@ -96,100 +68,67 @@ public class CdkStack extends Stack {
         userDataScript.addCommands("java -jar target/SpringVideoApp-1.0-SNAPSHOT.jar --server.port=8080");
 
 
-        Bucket.Builder.create(this, "aygo-bucket-project-test")
-                .bucketName("aygo-bucket-project-test")
-                .versioned(false)
-                .removalPolicy(RemovalPolicy.DESTROY)
-                .encryption(BucketEncryption.UNENCRYPTED)
-                .autoDeleteObjects(false)
-                .blockPublicAccess(BlockPublicAccess.BLOCK_ACLS) // Block all public access ??
-                .objectOwnership(ObjectOwnership.BUCKET_OWNER_PREFERRED)
-                .build();
-
-        //List<InstanceTarget> targets = new ArrayList<>();
-
-        /*for (int i = 0; i < 3; i++) {
-            Instance instance = Instance.Builder.create(this, "aygo-project-" + i)
-                    .instanceType(instanceType)
-                    .machineImage(machineImage)
-                    .vpc(vpc)
-                    .vpcSubnets(subnetSelection)
-                    .allowAllOutbound(true)
-                    .securityGroup(securityGroup)
-                    .userData(userDataScript)
-                    .keyName("aygo-key")
-                    .build();
-            InstanceTarget instanceTarget = new InstanceTarget(instance);
-            targets.add(instanceTarget);
-        }*/
+        Bucket.Builder.create(this, "aygo-bucket-project-test").bucketName("aygo-bucket-project-test").versioned(false).removalPolicy(RemovalPolicy.DESTROY).encryption(BucketEncryption.UNENCRYPTED).autoDeleteObjects(false).blockPublicAccess(BlockPublicAccess.BLOCK_ACLS) // Block all public access ??
+                .objectOwnership(ObjectOwnership.BUCKET_OWNER_PREFERRED).build();
 
         //auto scaling group
-        AutoScalingGroup autoScalingGroup = AutoScalingGroup.Builder.create(this, "AutoScalingGroup")
-                .instanceType(instanceType)
-                .machineImage(machineImage)
-                .vpc(vpc)
-                .vpcSubnets(subnetSelection)
-                .allowAllOutbound(true)
-                .securityGroup(securityGroup)
-                .userData(userDataScript)
-                .keyName("aygo-key")
-                .minCapacity(1)
-                .maxCapacity(3)
-                .build();
+        AutoScalingGroup autoScalingGroup = AutoScalingGroup.Builder.create(this, "AutoScalingGroup").instanceType(instanceType).machineImage(machineImage).vpc(vpc).vpcSubnets(subnetSelection).allowAllOutbound(true).securityGroup(securityGroup).userData(userDataScript).keyName("aygo-key").minCapacity(1).maxCapacity(3).build();
 
 
-
-
-        HealthCheck healthCheck = HealthCheck
-                .builder()
-                .enabled(true)
-                .protocol(Protocol.HTTP)
-                .port("8080")
-                .build();
+        HealthCheck healthCheck = HealthCheck.builder().enabled(true).protocol(Protocol.HTTP).port("8080").build();
 
         // 👇 add target to the ALB listener autoScalingGroup as target
-        ApplicationTargetGroup targetGroup = ApplicationTargetGroup.Builder.create(this, "aygo-project-target-group")
-                .targetGroupName("aygo-project-target-group")
-                .targetType(TargetType.INSTANCE)
-                .protocol(ApplicationProtocol.HTTP)
-                .port(8080)
-                .vpc(vpc)
-                .protocolVersion(ApplicationProtocolVersion.HTTP1)
-                .healthCheck(healthCheck)
-                .targets(Collections.singletonList(autoScalingGroup))
-                .build();
+        ApplicationTargetGroup targetGroup = ApplicationTargetGroup.Builder.create(this, "aygo-project-target-group").targetGroupName("aygo-project-target-group").targetType(TargetType.INSTANCE).protocol(ApplicationProtocol.HTTP).port(8080).vpc(vpc).protocolVersion(ApplicationProtocolVersion.HTTP1).healthCheck(healthCheck).targets(Collections.singletonList(autoScalingGroup)).build();
 
 
-        ApplicationLoadBalancer loadBalancer = ApplicationLoadBalancer.Builder.create(this, "aygo-project-load-balancer")
-                .loadBalancerName("aygo-project-load-balancer")
-                .ipAddressType(IpAddressType.IPV4)
-                .vpc(vpc)
-                .internetFacing(true)
-                .vpcSubnets(subnetSelection)
-                .securityGroup(securityGroup)
-                .build();
+        ApplicationLoadBalancer loadBalancer = ApplicationLoadBalancer.Builder.create(this, "aygo-project-load-balancer").loadBalancerName("aygo-project-load-balancer").ipAddressType(IpAddressType.IPV4).vpc(vpc).internetFacing(true).vpcSubnets(subnetSelection).securityGroup(securityGroup).build();
 
 
         ApplicationListener listener = loadBalancer.addListener("listener", BaseApplicationListenerProps.builder().port(8080).build());
-        listener.addTargetGroups("targetGroup", AddApplicationTargetGroupsProps.builder()
-                        .targetGroups(Arrays.asList(targetGroup))
+        listener.addTargetGroups("targetGroup", AddApplicationTargetGroupsProps.builder().targetGroups(Arrays.asList(targetGroup)).build());
+
+
+        HttpApi appi = HttpApi.Builder.create(this, "aygo-project-http-api")
+                .apiName("aygo-project-http-api")
+                .corsPreflight(CorsPreflightOptions.builder()
+                        .allowOrigins(Arrays.asList("*"))
+                        .allowMethods(Arrays.asList(CorsHttpMethod.GET, CorsHttpMethod.POST, CorsHttpMethod.OPTIONS))
+                        .allowHeaders(Arrays.asList("*"))
+                        .build()).build();
+
+        //HttpUrlIntegration  for /
+        HttpUrlIntegration httpUrlIntegration = new HttpUrlIntegration("root", "http://" + loadBalancer.getLoadBalancerDnsName() + ":8080");
+
+        //HttpUrlIntegration for /items
+        HttpUrlIntegration httpUrlIntegrationItems = new HttpUrlIntegration("items", "http://" + loadBalancer.getLoadBalancerDnsName() + ":8080/items");
+
+        //http integration for /fileupload post
+        HttpUrlIntegration urlIntegrationFileUpLoad = new HttpUrlIntegration("fileupload", "http://" + loadBalancer.getLoadBalancerDnsName() + ":8080/fileupload");
+
+        //http integration for /{id}/stream get
+        HttpUrlIntegration urlIntegrationStream = new HttpUrlIntegration("stream", "http://" + loadBalancer.getLoadBalancerDnsName() + ":8080/{id}/stream");
+
+
+        appi.addRoutes(AddRoutesOptions.builder().path("/")
+                .methods(Arrays.asList(HttpMethod.GET, HttpMethod.OPTIONS))
+                .integration(httpUrlIntegration)
+                .build());
+
+        appi.addRoutes(AddRoutesOptions.builder().path("/items")
+                .methods(Arrays.asList(HttpMethod.GET, HttpMethod.OPTIONS))
+                .integration(httpUrlIntegrationItems)
+                .build());
+
+        appi.addRoutes(AddRoutesOptions.builder().path("/fileupload")
+                .methods(Arrays.asList(HttpMethod.POST, HttpMethod.OPTIONS))
+                .integration(urlIntegrationFileUpLoad)
+                .build());
+
+        appi.addRoutes(AddRoutesOptions.builder().path("/{id}/stream")
+                .methods(Arrays.asList(HttpMethod.GET, HttpMethod.OPTIONS))
+                .integration(urlIntegrationStream)
                 .build());
 
 
-        //api gateway http api from load balancer
-        HttpApi.Builder.create(this, "aygo-project-api-gateway")
-                .apiName("aygo-project-api-gateway")
-                .corsPreflight(CorsPreflightOptions.builder()
-                        .allowOrigins(Arrays.asList("*"))
-                        //.allowMethods(Arrays.asList(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE))
-                        .allowHeaders(Arrays.asList("*"))
-                        .build())
-                .build();
-
-
-
-        //
-
-        topic.addSubscription(new SqsSubscription(queue));
     }
 }
--- cdk/src/test/java/com/myorg/CdkStackTest.java
+++ cdk/src/test/java/com/myorg/CdkStackTest.java
@@ -13,15 +13,7 @@ public class CdkStackTest {
 
     @Test
     public void testStack() throws IOException {
-        App app = new App();
-        CdkStack stack = new CdkStack(app, "test");
 
-        Template template = Template.fromStack(stack);
 
-        template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
-          put("VisibilityTimeout", 300);
-        }});
-
-        template.resourceCountIs("AWS::SNS::Topic", 1);
     }
 }
