4c78646d425f2300d4c08443d0782910bff8089d
JulianBenitez <julian.benitezg99@gmail.com>
2022-11-29 19:08:47 -0500

    Update url

--- frontend/config/config.tsx
+++ frontend/config/config.tsx
@@ -1,5 +1,5 @@
 const config = {
-  url: 'https://ki5vuq3ws5.execute-api.us-east-1.amazonaws.com',
+  url: 'https://b5bzu658y0.execute-api.us-east-1.amazonaws.com',
   cognito_hosted_domain: 'aygo-vs.auth.us-east-1.amazoncognito.com',
   redirect_url: 'https://master.d25pbvsdf15l9z.amplifyapp.com/',
   cognito_client_id: '11kn6p0hdacm1ojdsvg9lqqpb7',
